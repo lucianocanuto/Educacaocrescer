ba3df5f53b2eced96282292ce0bd3bf9603d9abb
Luciano <luciano.canuto@outlook.com.br>
2025-06-17 18:52:45 -0300

    Botão cadastrar professor com senha para ir para a tela cadastrar professor

--- app/src/main/java/br/com/educacaocrescer/educacaocrescer/MainActivity.kt
+++ app/src/main/java/br/com/educacaocrescer/educacaocrescer/MainActivity.kt
@@ -1,21 +1,50 @@
 package br.com.educacaocrescer.educacaocrescer
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.com.educacaocrescer.educacaocrescer.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.btnCadastrarProfessor.setOnClickListener {
+            val input = EditText(this)
+            input.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+            AlertDialog.Builder(this)
+                .setTitle("Autenticação necessária")
+                .setMessage("Digite a senha da diretora para continuar:")
+                .setView(input)
+                .setPositiveButton("OK") { _, _ ->
+                    val senhaDigitada = input.text.toString()
+                    val senhaDiretora = "admin123" // Você pode mover isso para o Firebase futuramente
+
+                    if (senhaDigitada == senhaDiretora) {
+                        val intent = Intent(this, CadastroProfessorActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Senha incorreta!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .setNegativeButton("Cancelar", null)
+                .show()
         }
+
+
     }
 }
